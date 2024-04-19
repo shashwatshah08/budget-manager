5c3f04d8ff66d03ce4e434816184507e8623d4cd
Shashwat Shah <shashwatshah@shashwats-air.home>
2024-04-19 18:20:07 -0300

    Added some content in the page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,29 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="prose ">
+        <h1 className="whitespace-nowrap pt-10">
+          Turn Your Money Frowns Upside Down!
+        </h1>
+        <h2 className="mt-5">What we offer:</h2>
+        <ul className="list-inside list-none">
+          <li>
+            <strong>Expense tracking:</strong> Our expense tracking feature
+            keeps your spending in check, so you can splurge guilt-free on that
+            extra cup of coffee!
+          </li>
+          <li>
+            <strong>Data Visualization:</strong> Turn boring numbers into
+            stunning visuals! Our data visualization tool makes graphs and
+            charts so beautiful, you'll want to frame them on your wall!
+          </li>
+          <li>
+            <strong>Add Friends and Split Money:</strong> Friends who split
+            bills together, stay together! Easily divide expenses with pals and
+            never argue about who owes what again!
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
