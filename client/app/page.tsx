c01d348b5f97588363d3ade37ed830fa6fb476b2
Shashwat Shah <shashwatshah@shashwats-air.home>
2024-04-20 01:10:44 -0300

    Added zoom animation to the elements.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,28 +5,30 @@ export default function Home() {
     <div className="flex h-screen">
       <div className="w-[60%] h-full flex flex-col items-center justify-center">
         <div className="prose text-justify">
-          <h1 className="whitespace-nowrap">
+          <h1 className="whitespace-nowrap transform transition duration-500 ease-in-out hover:scale-110">
             Turn Your Money Frowns Upside Down!
           </h1>
-          <h2 className="mt-5">What we offer:</h2>
-          <ul className="list-inside list-none text-2xl">
-            <li>
+          <h2 className="mt-5 transform transition duration-500 ease-in-out hover:scale-110">
+            What we offer:
+          </h2>
+          <ul className="list-inside list-none text-xl space-y-4">
+            <li className="transform transition duration-500 ease-in-out hover:scale-110">
               <strong>Expense tracking:</strong> Our expense tracking feature
               keeps your spending in check, so you can splurge guilt-free on
               that extra cup of coffee! ☕️
             </li>
-            <li>
+            <li className="transform transition duration-500 ease-in-out hover:scale-110">
               <strong>Data Visualization:</strong> Turn boring numbers into
               stunning visuals! 📊 Our data visualization tool makes graphs and
               charts so beautiful, you'll want to frame them on your wall! 😉
             </li>
-            <li>
+            <li className="transform transition duration-500 ease-in-out hover:scale-110">
               <strong>Add Friends and Split Money:</strong> Friends who split
               bills together, stay together! Easily divide expenses with pals
               and never argue about who owes what again! 💸
             </li>
           </ul>
-          <p className="mt-5 text-xl">
+          <p className="mt-5 text-xl transform transition duration-500 ease-in-out hover:scale-110">
             Our current features will make you go <strong>WOW</strong> and guess
             what? We're cooking up even more magic to elevate your financial
             game! To unlock the full experience and get first dibs on our
@@ -36,7 +38,7 @@ export default function Home() {
         <button className="btn btn-neutral mt-5 w-1/4 text-lg rounded-full transform transition duration-200 ease-in-out hover:scale-110">
           Sign up
         </button>
-        <p className="mt-2">
+        <p className="mt-2 transform transition duration-500 ease-in-out hover:scale-110">
           Already registered?
           <Link href="/login">
             <span className="ml-2 text-black hover:underline cursor-pointer font-bold">
@@ -46,7 +48,7 @@ export default function Home() {
         </p>
       </div>
       <div className="w-2/5 h-full flex items-center justify-center mr-10">
-        <div className="card bg-base-200 shadow-xl w-[80%] h-[60%] rounded-2xl">
+        <div className="card bg-base-200 shadow-xl w-[80%] h-[60%] rounded-2xl transform transition duration-500 ease-in-out hover:scale-110">
           <img
             src="designer.svg"
             alt="designer"
